5b0b547adae213de5b27b0785a4fc4265b96cb49
ThiwankaS <thivankas@outlook.com>
2025-12-18 00:09:20 +0200

    string manupulation and user input

---
+++ string_manip.py
@@ -0,0 +1,16 @@
+# expecting user to input a name
+name = input("Enter your name : ")
+
+# untill user input is empty this will prompt
+while (len(name) == 0):
+    print("You are expect to enter your name !")
+    print("Enter 'Exit' to exit the program.")
+    name = input("Enter your name : ")
+
+# if user input exit then leave the program 
+if(name.lower() == "exit"):
+    print("ok, exiting the program!")
+# is user input aything other than 'Exit' will consider as a valid name
+else:
+    name = "Mr." + name.capitalize()
+    print ("Hello, ", name, end="\n")
